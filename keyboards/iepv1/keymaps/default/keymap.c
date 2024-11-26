54998d8958b05cde7b4f0c5b3a63227bee701f3e
eric <ericzhao96@hotmail.com>
2024-11-26 19:16:56 +0800

    feat: stash

--- keyboards/iepv1/keymaps/default/keymap.c
+++ keyboards/iepv1/keymaps/default/keymap.c
@@ -11,6 +11,7 @@ enum layer_names {
     _QWERTY,
     _MAC,
     _CO,
+    _FN,
 };
 
 enum my_keycodes {
@@ -31,15 +32,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_N,               KC_M,           KC_COMM,        KC_DOT,          KC_SLASH,       KC_RIGHT_SHIFT,
         LT(_CO, KC_ENTER),  KC_SPACE,       A(KC_LSFT),     KC_CAPS,         DF(_COLEMAK),   TG(_CO)
     ),
-    [_CO] = LAYOUT_default(
-        _______,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,
-        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,   
+    [_FN] = LAYOUT_default(
+        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,
         KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
+        _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,
+
+        _______,        KC_VOLU,        _______,        _______,        _______,        _______,
+        _______,        KC_VOLD,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,
+    ),
+    [_CO] = LAYOUT_default(
+        KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,
+        _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,
         _______,        _______,        _______,        _______,        _______,        _______,
 
         KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BACKSLASH,
-        KC_VOLU,        KC_LBRC,        KC_RBRC,        _______,        KC_MINS,        KC_EQL,
-        KC_VOLD,        _______,        KC_HOME,        KC_UP,          KC_END,         _______,
+        _______,        KC_LBRC,        KC_RBRC,        _______,        KC_MINS,        KC_EQL,
+        _______,        _______,        KC_HOME,        KC_UP,          KC_END,         _______,
         _______,        _______,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       _______
     ),
     [_MAC] = LAYOUT_default(
@@ -54,15 +66,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        _______,        KC_CAPS,        S(KC_CAPS),     _______,        _______
     ),
     [_COLEMAK] = LAYOUT_default(
-        QK_GESC,        KC_Q,           KC_W,          KC_F,           KC_P,          KC_G,
+        KC_ESC,         KC_Q,           KC_W,          KC_F,           KC_P,          KC_G,
         KC_BACKSPACE,   KC_A,           KC_R,          KC_S,           KC_T,          KC_D,
         KC_LEFT_SHIFT,  KC_Z,           KC_X,          KC_C,           KC_V,          KC_B,
-        KC_LEFT_CTRL,   XXXXXXX,        KC_LWIN,       KC_LALT,        KC_TAB,        MO(_CO),
+        KC_LEFT_CTRL,   TG(_CO),        KC_LWIN,       KC_LALT,        KC_TAB,        MO(_CO),
 
         KC_J,               KC_L,           KC_U,           KC_Y,            KC_SCLN,         KC_DELETE,
         KC_H,               KC_N,           KC_E,           KC_I,            KC_O,            KC_QUOT,
         KC_K,               KC_M,           KC_COMM,        KC_DOT,          KC_SLASH,        KC_RIGHT_SHIFT,
-        LT(_CO, KC_ENTER),  KC_SPACE,       A(KC_LSFT),     KC_CAPS,         DF(_QWERTY),     TG(_CO) 
+        LT(_CO, KC_ENTER),  KC_SPACE,       A(KC_LSFT),     KC_CAPS,         DF(_QWERTY),     LT(_FN, )
     ),
 };
 
@@ -147,7 +159,7 @@ bool process_detected_host_os_kb(os_variant_t detected_os) {
         case OS_UNSURE:
             break;
     }
-    
+
     return true;
 }
 
